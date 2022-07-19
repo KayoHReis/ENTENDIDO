1d1f162e7ce9ad929f83ab8a13eb920a817e8a49
KayoHReis <kayo.reis@alunos.ifsuldeminas.edu.br>
2022-07-19 18:54:45 -0300

    principal

---
+++ entendid/src/main/java/entendido/Entendido.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entendido;
+
+/**
+ *
+ * @author kayop
+ */
+public class Entendido {
+    public static void main(String[] args) {
+        
+    }
+}
