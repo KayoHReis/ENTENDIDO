69cd9691edef6b0aee483c72fc3fe077e319d235
KayoHReis <kayo.reis@alunos.ifsuldeminas.edu.br>
2022-07-19 19:50:27 -0300

    principal

--- entendid/src/main/java/entendido/Entendido.java
+++ entendid/src/main/java/entendido/Entendido.java
@@ -8,8 +8,17 @@ package entendido;
  *
  * @author kayop
  */
+import javax.swing.JOptionPane;
+
 public class Entendido {
     public static void main(String[] args) {
-        
+       
+        String nome[]= {"Não","Não"};
+       
+       int a= JOptionPane.showOptionDialog (null,"Entendido?", "teste", 0, 1, null,nome,1);
+        while(a == 0){
+            a= JOptionPane.showOptionDialog (null,"Entendido?", "teste", 0, 1, null,nome,1);
+        }
+       
     }
 }
